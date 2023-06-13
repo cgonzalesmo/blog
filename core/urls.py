31c4c5e8c230a00c955c532498b29b4b1e6022a1
Carlos Gonzales <cgonzalesmo@unsa.edu.pe>
2023-06-13 16:04:16 -0500

    Update project

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [    
+urlpatterns = [
+    path('api/blog/', include('apps.blog.urls')),
+    path('api/category/', include('apps.category.urls')),    
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
